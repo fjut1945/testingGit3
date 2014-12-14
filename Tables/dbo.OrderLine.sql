e7217ed4040bc9d67db395ae531057c17f7397a8
fjut1945 <>
2014-12-14 14:19:01 +0100

    Commited with ApexSQL Source Control

--- Tables/dbo.OrderLine.sql
+++ Tables/dbo.OrderLine.sql
@@ -1,13 +1,14 @@
 SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
-SET ANSI_PADDING OFF
+SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[OrderLine] (
 		[OrderId]        [int] NOT NULL,
 		[LineNumber]     [int] NOT NULL,
 		[ProductId]      [int] NOT NULL,
 		[Qty]            [numeric](18, 3) NOT NULL,
-		[LineTotal]      [numeric](18, 2) NOT NULL
+		[LineTotal]      [numeric](18, 2) NOT NULL,
+		[tedt]           [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[OrderLine]
